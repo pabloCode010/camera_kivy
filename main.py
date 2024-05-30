16e5dc46653e75cd46dd1da0e2bb405588da131b
Pablo <pablocode010@gmail.com>
2024-05-30 15:17:51 -0600

    use camelCase

--- main.py
+++ main.py
@@ -5,11 +5,13 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.factory import Factory
 
-Factory.register("CameraWidget", module="widgets.camera_widget")
+Factory.register("CameraWidget", module="widgets.cameraWidget")
+Factory.register("GaleryWidget", module="widgets.galeryWidget")
 
 class CameraApp(App):
 	def build(self):
 		Builder.load_file("kv_files/CameraWidget.kv")
+		Builder.load_file("kv_files/GaleryWidget.kv")
 		
 		screen_manager = Builder.load_file("screen_manager.kv")
 		return screen_manager
---
+++ widgets/galeryWidget.py
@@ -0,0 +1,5 @@
+from time import strftime
+from kivy.uix.widget import Widget
+
+class GaleryWidget(Widget):
+    pass
