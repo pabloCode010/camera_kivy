1c72ce4c0ecf5d455291566ce9a2780421a17e2b
Pablo <pablocode010@gmail.com>
2024-06-03 16:39:51 -0600

    show image on click

--- widgets/galleryImage.py
+++ widgets/galleryImage.py
@@ -1,4 +1,17 @@
 from kivy.uix.image import Image
+from kivy.uix.behaviors import ButtonBehavior
+from kivy.uix.popup import Popup
 
-class GalleryImage(Image):
-    pass
+class GalleryImage(ButtonBehavior, Image):
+    def show_image(self):
+        popup_content = Image(
+            source = self.source)
+
+        popup = Popup(
+            title = 'Image',
+            content = popup_content,
+            size_hint = (None, None),
+            size = (self.parent.width, 800)
+        )
+
+        popup.open()
